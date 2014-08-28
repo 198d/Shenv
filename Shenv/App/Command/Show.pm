5dfd1897c9b0d139e4796c6f5cdf8bc0beb6cbad
John MacKenzie <john@nineteeneightd.com>
2014-08-28 06:13:36 -0500

    --eval Option for show Command
    Add new option to the show command, --eval, that outputs export
    statemnts which can be used with eval to export the current environment
    as environment variables. This can be used with something like Bash's
    PROMPT_COMMAND to avoid having to use `shenv exec` to execute commands.

--- Shenv/App/Command/Show.pm
+++ Shenv/App/Command/Show.pm
@@ -6,14 +6,32 @@ use strict;
 use warnings;
 use feature 'say';
 
+use String::ShellQuote 'shell_quote';
+
 use Shenv::Client;
 use Shenv::App -command;
 
 
+sub opt_spec {
+    return (
+        ["eval|e", "output export statements to use in shell eval"]
+    )
+}
+
+
 sub execute {
+    my ($self, $opt, $args) = @_;
+
     my %environment = source();
+
     while (my ($name, $value) = each(%environment)) {
-        say("$name=$value");
+        if ($opt->{eval}) {
+            my $quoted_value = shell_quote($value);
+            say("export $name=$quoted_value;");
+        }
+        else {
+            say("$name=$value");
+        }
     }
 }
 
